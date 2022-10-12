ef06b1e965056fcae783e919c00f52ad9a71299e
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-11 22:59:20 -0700

    fail the second test function

--- ServerSide/testAllQuotes.js
+++ ServerSide/testAllQuotes.js
@@ -15,14 +15,14 @@ let aq01 = new AllQuotes();
 aq01.addQuote("Every man is guilty of all the good he didn't do.");
 aq01.addQuote("Before you embark on a journey of revenge, dig two graves.");
 let res01 = aq01.findQuote("graves");
-if (res01.search("Before you embark yadda yadda") > -1){
+if (res01.search("Before you embark") > -1){
     logPass("T01");
 } else {
     logFail("T01");
 }
 // T02 - Search for a quote and fail
 let res02 = aq01.findQuote("missing");
-if (res02.search("No matching quote") > -1){
+if (res02.search("No matching quote123") > -1){
     logPass("T02");
 } else {
     logFail("T02");
