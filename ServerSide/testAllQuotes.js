21e858ecbd9c5598384ead4865a8237d5c64458a
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-11 23:17:14 -0700

    Refactored the test cases

--- ServerSide/testAllQuotes.js
+++ ServerSide/testAllQuotes.js
@@ -8,31 +8,51 @@ function logPass(tName){
 }
 function logFail(tName){
     console.log("FAIL - " + tName )
-    throw ('test error code',tName);
+    throw new Error("failed test: " + tName)
 }
+
+function runtests(){
+
+    runtest1();
+    runtest2();
+    runtest3();
+
+}
+
 // T01 - Search for a quote and suceed
-let aq01 = new AllQuotes();
-aq01.addQuote("Every man is guilty of all the good he didn't do.");
-aq01.addQuote("Before you embark on a journey of revenge, dig two graves.");
-let res01 = aq01.findQuote("graves");
-if (res01.search("Before you embark") > -1){
-    logPass("T01");
-} else {
-    logFail("T01");
+function runtest1(){
+    let aq01 = new AllQuotes();
+    aq01.addQuote("Every man is guilty of all the good he didn't do.");
+    aq01.addQuote("Before you embark on a journey of revenge, dig two graves.");
+    let res01 = aq01.findQuote("graves");
+    if (res01.search("Before you embark") > -1){
+        logPass("T01");
+    } else {
+        logFail("T01");
+    }
 }
+
 // T02 - Search for a quote and fail
-let res02 = aq01.findQuote("missing");
-if (res02.search("No matching quote") > -1){
-    logPass("T02");
-} else {
-    logFail("T02");
+function runtest2(){
+    let aq01 = new AllQuotes();
+    let res02 = aq01.findQuote("missing");
+    if (res02.search("No matching quote") > -1){
+        logPass("T02");
+    } else {
+        logFail("T02");
+    }
 }
+
 // T03 - Load from file
-let aq03 = new AllQuotes();
-aq03.loadFromFile("../data/3_quotes.csv");
-let res03 = aq03.findQuote("smiles");
-if (res03.search("indicate where smiles have been123") > -1){
-    logPass("T03");
-} else {
-    logFail("T03");
+function runtest3(){
+    let aq03 = new AllQuotes();
+    aq03.loadFromFile("../data/3_quotes.csv");
+    let res03 = aq03.findQuote("smiles");
+    if (res03.search("indicate where smiles have been") > -1){
+        logPass("T03");
+    } else {
+        logFail("T03");
+    }
 }
+
+runtests()
