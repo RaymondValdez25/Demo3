8f73826d6749ea333d1f34420fd14192059a77f6
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-15 19:31:19 -0700

    removed the found comment

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # AMI for AWS LINUX in the us-east-2 region
-export AWS_LINUX_AMI="ami-0ca622d21decca9ad" Where is this found?
+export AWS_LINUX_AMI="ami-0ca622d21decca9ad"
 # Name of a key pair I will use to log in
-export MY_KEY_PAIR="PuTTYKeyPair1" Where is this found?
+export MY_KEY_PAIR="PuTTYKeyPair1"
 # Public subnet in my VPC in us-east-2 region
-export MY_PUBLIC_SUBNET="subnet-080c245137fc1dbc8" Where is this found?
+export MY_PUBLIC_SUBNET="subnet-080c245137fc1dbc8"
 # Security group for that VPC providing port 22 and 3000 access
-export MY_SECURITY_GROUP="sg-0cddccc77ca099d24" Where is this found?
+export MY_SECURITY_GROUP="sg-0cddccc77ca099d24"
 #Remove the dryrun flag when everything seems to be ready
 aws --region us-east-2 ec2 run-instances \
  --dry-run \
