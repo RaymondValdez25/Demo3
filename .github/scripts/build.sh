82d710b4acf3aa9f60823368f7bd25a18866abe4
Raymond Valdez <40275318+RaymondValdez25@users.noreply.github.com>
2022-10-15 20:10:45 -0700

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -8,10 +8,10 @@ export MY_PUBLIC_SUBNET="subnet-080c245137fc1dbc8"
 # Security group for that VPC providing port 22 and 3000 access
 export MY_SECURITY_GROUP="sg-0cddccc77ca099d24"
 #Remove the dryrun flag when everything seems to be ready
-aws --region us-east-2 ec2 run-instances \
- --dry-run \
- --image-id $AWS_LINUX_AMI --count 1 \
- --instance-type t2.micro \
- --subnet-id $MY_PUBLIC_SUBNET \
- --security-group-ids $MY_SECURITY_GROUP \
- --key-name $MY_KEY_PAIR
+#aws --region us-east-2 ec2 run-instances \
+# --dry-run \
+# --image-id $AWS_LINUX_AMI --count 1 \
+# --instance-type t2.micro \
+# --subnet-id $MY_PUBLIC_SUBNET \
+# --security-group-ids $MY_SECURITY_GROUP \
+# --key-name $MY_KEY_PAIR
