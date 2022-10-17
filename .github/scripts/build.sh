842774c6dd1fb6bfe7e42a83ac8150c80a298242
Raymond Valdez <40275318+RaymondValdez25@users.noreply.github.com>
2022-10-17 16:50:08 -0700

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -14,7 +14,7 @@ echo $MY_SECURITY_GROUP
 echo $MY_KEY_PAIR
  
 #Remove the dryrun flag when everything seems to be ready
-aws --region us-east-2 ec2 run-instances \
+listOfInstances = aws --region us-east-2 ec2 run-instances \
  --image-id $AWS_LINUX_AMI --count 1 \
  --instance-type t2.micro \
  --subnet-id $MY_PUBLIC_SUBNET \
@@ -22,5 +22,6 @@ aws --region us-east-2 ec2 run-instances \
  --key-name $MY_KEY_PAIR \
  --associate-public-ip-address
 
+printf '${listOfInstances["Instances"][0]["InstanceId"]}'
 
 
