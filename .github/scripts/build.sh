8271e15579bfa785561799635348d222881f0491
Raymond Valdez <40275318+RaymondValdez25@users.noreply.github.com>
2022-10-17 17:34:18 -0700

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -7,6 +7,8 @@ export MY_KEY_PAIR="PuTTYKeyPair1"
 export MY_PUBLIC_SUBNET="subnet-080c245137fc1dbc8"
 # Security group for that VPC providing port 22 and 3000 access
 export MY_SECURITY_GROUP="sg-08f82f6a506a00310"
+#my instance name
+export TAGS="ResourceType=instance,Tags=[{Key=Name,Value=Test_Quote_Server}]"
 
 echo $AWS_LINUX_AMI
 echo $MY_PUBLIC_SUBNET
@@ -14,16 +16,14 @@ echo $MY_SECURITY_GROUP
 echo $MY_KEY_PAIR
  
 #Remove the dryrun flag when everything seems to be ready
-GroupsAndInstances=$(aws --region us-east-2 ec2 run-instances \
+aws --region us-east-2 ec2 run-instances \
  --image-id $AWS_LINUX_AMI --count 1 \
  --instance-type t2.micro \
  --subnet-id $MY_PUBLIC_SUBNET \
  --security-group-ids $MY_SECURITY_GROUP \
  --key-name $MY_KEY_PAIR \
- --associate-public-ip-address)
- 
- InstanceID=${GroupsAndInstances["Instances"][0]["InstanceId"]}
+ --tag-specifications $TAGS
 
-echo "the instanceID is $InstanceID"
+
 
 
