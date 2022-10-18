350a5e53a0476070621742081a25b7dc16a707ae
Raymond Valdez <40275318+RaymondValdez25@users.noreply.github.com>
2022-10-18 10:24:23 -0700

    Update user-script.sh

--- .github/scripts/user-script.sh
+++ .github/scripts/user-script.sh
@@ -4,6 +4,10 @@ aws s3 cp "s3://raymondsdemo3workbucket/deployment.tar.gz" \
 deployment.tar.gz
 gunzip deployment.tar.gz
 tar -xf deployment.tar
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
 npm install pm2@latest -g
 cd ServerSide
 pm2 start server.js
