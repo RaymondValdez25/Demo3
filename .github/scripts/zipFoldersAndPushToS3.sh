b881bd28ad793c44685af5f26f65ce16b13236d7
Raymond Valdez <40275318+RaymondValdez25@users.noreply.github.com>
2022-10-17 19:26:58 -0700

    Update zipFoldersAndPushToS3.sh

--- .github/scripts/zipFoldersAndPushToS3.sh
+++ .github/scripts/zipFoldersAndPushToS3.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
 pwd
-tar -cvf deployment.tar data serverSide
+tar -cvf deployment.tar data ServerSide
 gzip deployment.tar
 aws s3 --dryrun cp deployment.tar.gz\
  "s3://raymondsdemo3workbucket/deployment.tar.gz"
