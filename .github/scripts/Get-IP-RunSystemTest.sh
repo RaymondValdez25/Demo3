37dc9a93aef9343a9e5df8529d1a206d575ab166
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-22 16:39:10 -0700

    only run the system test on running instances

--- .github/scripts/Get-IP-RunSystemTest.sh
+++ .github/scripts/Get-IP-RunSystemTest.sh
@@ -3,8 +3,8 @@
 publicIP=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
---query Reservations[].Instances[].PublicIpAddress \
---filters "Name=tag:Name,Values=Test_Quote_Server" \
+--query Reservations[].Instances[].InstanceId \
+--filters "Name=tag:Name,Values=Test_Quote_Server" "Name=instance-state-name,Values=running" \
 --output=text)
 sleep 120
 curl http://$publicIP:3000/search?search_string=graves
