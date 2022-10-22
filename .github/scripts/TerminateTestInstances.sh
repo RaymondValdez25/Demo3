70b8f01da0550446c54d84135b0ae9db52fd2d09
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-22 16:15:37 -0700

    terminating all test and prod instances

--- .github/scripts/TerminateTestInstances.sh
+++ .github/scripts/TerminateTestInstances.sh
@@ -3,7 +3,8 @@ Instances=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].InstanceId  \
---filters "Name=tag:Name,Values=Prod_Quote_Server" \
+--filters "Name=tag:Name,Values=Test_Quote_Server" \
 --output=text)
+
 echo $Instances
 aws ec2 terminate-instances --instance-ids $Instances
