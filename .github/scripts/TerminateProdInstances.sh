6a52dc4f55cabd581e6c16fba3f5b20c69d75386
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-22 15:54:52 -0700

    cleanup Prod instances

---
+++ .github/scripts/TerminateProdInstances.sh
@@ -0,0 +1,12 @@
+#!/bin/bash        
+        
+Instances=$(aws ec2 describe-instances \
+--region us-east-2 \
+--instance-ids \
+--query Reservations[].Instances[].InstanceId  \
+--filters "Name=tag:Name,Values=Prod_Quote_Server" \
+--output=text)
+
+echo $Instances
+
+aws ec2 terminate-instances --instance-ids $Instances
