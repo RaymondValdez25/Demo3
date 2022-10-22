96e03f30c164083ee11c2003e8fccc3419b23add
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-22 16:50:37 -0700

    testing public IPs

--- .github/scripts/Get-IP-RunSystemTest.sh
+++ .github/scripts/Get-IP-RunSystemTest.sh
@@ -4,7 +4,7 @@ publicIP=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].InstanceId \
---filters "Name=tag:Name,Values=Test_Quote_Server" "Name=instance-state-name,Values=running" \
+--filters "Name=tag:Name,Values=Test_Quote_Server" "Name=instance-state-name,Values=running | pending" \
 --output=text)
 #sleep 120
 
