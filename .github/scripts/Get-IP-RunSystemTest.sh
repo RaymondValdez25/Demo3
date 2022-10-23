4a217f6d26f00d1e33fc4b819971e10b4d5a0bd0
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-22 17:02:44 -0700

    cleanup termination of EC2 instances

--- .github/scripts/Get-IP-RunSystemTest.sh
+++ .github/scripts/Get-IP-RunSystemTest.sh
@@ -6,13 +6,8 @@ publicIP=$(aws ec2 describe-instances \
 --query Reservations[].Instances[].InstanceId \
 --filters Name=tag:Name,Values=Test_Quote_Server Name=instance-state-name,Values=running,pending \
 --output=text)
-#sleep 120
+sleep 120
 
 echo echoing IPs $publicIP
 
-#curl http://$publicIP:3000/search?search_string=graves
-
-
-
-aws ec2 describe-instances \
-    --filters Name=instance-type,Values=t2.micro,t3.micro Name=availability-zone,Values=us-east-2c
+curl http://$publicIP:3000/search?search_string=graves
--- .github/scripts/TerminateTestInstances.sh
+++ .github/scripts/TerminateTestInstances.sh
@@ -3,7 +3,7 @@ Instances=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].InstanceId  \
---filters "Name=tag:Name,Values=Test_Quote_Server" \
+--filters Name=tag:Name,Values=Test_Quote_Server Name=instance-state-name,Values=running,pending,stopped,stopping \
 --output=text)
 
 echo $Instances
