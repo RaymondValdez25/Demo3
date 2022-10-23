af8c572db5cfa5cc8405ab54db745041a792541c
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-23 01:15:32 -0700

    cleanup and commit, passing test

--- .github/scripts/Get-IP.sh
+++ .github/scripts/Get-IP.sh
@@ -1,11 +1,12 @@
 #!/bin/bash  
-        
+
+sleep 120     
 publicIP=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].PublicIpAddress  \
 --filters Name=tag:Name,Values=Test_Quote_Server Name=instance-state-name,Values=running,pending \
 --output=text)
-sleep 120
+
 
 echo "EC2_PUBLIC_IP=$publicIP" >> $GITHUB_ENV
