ffa01296e529e17b2ffede9ba5df583b84334483
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-25 16:05:07 -0700

    push

--- .github/scripts/User-Script.sh
+++ .github/scripts/User-Script.sh
@@ -4,7 +4,7 @@ aws s3 cp "s3://raymondsdemo3workbucket/deployment.tar.gz" \
 deployment.tar.gz
 gunzip deployment.tar.gz
 tar -xf deployment.tar
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 . ~/.nvm/nvm.sh
