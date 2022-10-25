a97ab866ae99f793e97009a7d87544f441d417af
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-25 15:37:37 -0700

    committing

--- .github/scripts/User-Script.sh
+++ .github/scripts/User-Script.sh
@@ -7,8 +7,8 @@ tar -xf deployment.tar
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm use --delete-prefix v18.12.0
 nvm install --lts
+nvm use --delete-prefix v18.12.0
 npm install pm2@latest -g
 cd ServerSide
 pm2 start server.js
