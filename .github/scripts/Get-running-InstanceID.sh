b5f34e52e727b0e3a5775f5ae14627b50ebf8499
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-24 20:41:51 -0700

    committing

--- .github/scripts/Get-running-InstanceID.sh
+++ .github/scripts/Get-running-InstanceID.sh
@@ -1,11 +1,11 @@
 #!/bin/bash  
 
-Instance_ID=$(aws ec2 describe-instances \
+EC2_INSTANCE_ID=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].InstanceId  \
 --filters Name=tag:Name,Values=Prod_Quote_Server Name=instance-state-name,Values=running,pending \
 --output=text)
 
-echo $Instance_ID
-echo "Instance_ID=$Instance_ID" >> $GITHUB_ENV
+echo $EC2_INSTANCE_ID
+echo "EC2_INSTANCE_ID=$EC2_INSTANCE_ID" >> $GITHUB_ENV
