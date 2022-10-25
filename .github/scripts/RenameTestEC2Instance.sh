0fc8372d7d1f832c64594c2b8494acc873de587f
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-24 19:05:56 -0700

    testing rename EC2 instance

---
+++ .github/scripts/RenameTestEC2Instance.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+EC2_ID=$(aws ec2 describe-instances \
+--region us-east-2 \
+--instance-ids \
+--query Reservations[].Instances[].InstanceId  \
+--filters Name=tag:Name,Values=Test_Quote_Server Name=instance-state-name,Values=running,pending \
+--output=text)
+
+
+echo EC2_ID
+aws ec2 create-tags --resources $EC2_ID --tag "Key=Name,Value=Prod_Quote_Server
