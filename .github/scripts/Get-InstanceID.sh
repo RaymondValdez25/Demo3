b83a5883b52411abe4a9a5f8cc33cb7e0b973547
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-24 20:16:09 -0700

    refactor and testing exporting and associating

--- .github/scripts/Get-InstanceID.sh
+++ .github/scripts/Get-InstanceID.sh
@@ -1,16 +1,10 @@
 #!/bin/bash  
 
-EC2_ID=$(aws ec2 describe-instances \
+Instance_ID=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].InstanceId  \
 --filters Name=tag:Name,Values=Prod_Quote_Server Name=instance-state-name,Values=running,pending \
 --output=text)
 
-echo "EC2_INSTANCE_ID=$EC2_ID" >> $GITHUB_ENV
-
-aws ec2 modify-instance-attribute \
---instance-id $EC2_ID \
---groups sg-0329da65af5ecbc8b
-
-
+echo "Instance_ID=$Instance_ID" >> $GITHUB_ENV
