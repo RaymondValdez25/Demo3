c99ae5f272ee0b54744bd20ed4b3f693f296a5bc
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-25 16:09:04 -0700

    fix

--- .github/scripts/User-Script.sh
+++ .github/scripts/User-Script.sh
@@ -9,6 +9,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 . ~/.nvm/nvm.sh
 nvm install --lts
+nvm use --delete-prefix v18.12.0
 node -e "console.log('Running Node.js ' + process.version)"
 npm install pm2@latest -g
 cd ServerSide
