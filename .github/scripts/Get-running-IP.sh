5e52dd60452c2b19bbfd63974d08e5dfba286ce1
Raymond Valdez (INFOSYS LIMITED) <v-ravald@microsoft.com>
2022-10-24 20:51:02 -0700

    fix

--- .github/scripts/Get-running-IP.sh
+++ .github/scripts/Get-running-IP.sh
@@ -4,8 +4,8 @@ publicIP=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].PublicIpAddress  \
---filters Name=tag:Name,Values=Test_Quote_Server Name=instance-state-name,Values=running,pending \
+--filters Name=tag:Name,Values=${Environment}_Quote_Server Name=instance-state-name,Values=running,pending \
 --output=text)
 
-
+echo $publicIP
 echo "EC2_PUBLIC_IP=$publicIP" >> $GITHUB_ENV
--- .github/scripts/Get-running-InstanceID.sh
+++ .github/scripts/Get-running-InstanceID.sh
@@ -4,7 +4,7 @@ EC2_INSTANCE_ID=$(aws ec2 describe-instances \
 --region us-east-2 \
 --instance-ids \
 --query Reservations[].Instances[].InstanceId  \
---filters Name=tag:Name,Values=Prod_Quote_Server Name=instance-state-name,Values=running,pending \
+--filters Name=tag:Name,Values=${Environment}_Quote_Server Name=instance-state-name,Values=running,pending \
 --output=text)
 
 echo $EC2_INSTANCE_ID
