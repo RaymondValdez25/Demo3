3030059b832fa514273db2cc229c4180a7188fe9
Raymond Valdez <40275318+RaymondValdez25@users.noreply.github.com>
2022-11-01 22:22:59 -0700

    Update User-Script.sh

--- .github/scripts/User-Script.sh
+++ .github/scripts/User-Script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 aws s3 cp "s3://raymondsdemo3workbucket/deployment.tar.gz" \
 deployment.tar.gz
 gunzip deployment.tar.gz
 tar -xf deployment.tar
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 npm install pm2@latest -g
 cd ServerSide
